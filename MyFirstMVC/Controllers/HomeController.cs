f22d1aed5d60b97a305dbc39e857baf671d08339
TanjuSimsek <tanju.simsek@yandex.com>
2019-02-12 11:35:43 +0300

    MvcFirstApp

--- MyFirstMVC/Controllers/HomeController.cs
+++ MyFirstMVC/Controllers/HomeController.cs
@@ -16,8 +16,12 @@ namespace MyFirstMVC.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ViewData["Message"] = "my message";
+            TempData["Message"] = "gecici bilgi";
+            int deg = 15;
+            return View(deg);
 
-            return View();
+           
         }
 
         public ActionResult Contact()
@@ -26,5 +30,76 @@ namespace MyFirstMVC.Controllers
 
             return View();
         }
+       
+        public ActionResult TahminEt(int? tahmin)
+        {
+            if (Session["TuttugumSayi"] == null) {
+                Session["TuttugumSayi"] = new Random().Next(10) + 1;
+
+            }
+
+            if (tahmin.HasValue) {
+
+               
+               
+
+                if (tahmin == int.Parse(Session["TuttugumSayi"].ToString()))
+                {
+
+                    // return Content("Tebrikler,bildiniz");
+                    ViewBag.Message = "Tebrikler,Bildiniz";
+                    ViewBag.IsSuccess = true;
+                }
+                else
+                {
+
+                    // return Content($"Malesef bilemediniz :(Tutugum Sayi{tutugumSayi} idi.");
+                    // ViewBag.Message = $"Malesef bilemediniz sayi{tutugumSayi} idi.";
+                    ViewBag.Message = "Malesef Bilemediniz :( Lütfen Tekrar Deneyiniz";
+                    ViewBag.IsSuccess = false;
+                }
+
+
+
+            }
+            return View();
+        }
+        public ActionResult Replay()
+        {
+            Session["TuttugumSayi"] = new Random().Next(10) + 1;
+            return RedirectToAction("TahminEt");
+
+
+            
+        }
+
+
+
+
+        /*
+         *
+         *  public ActionResult TahminEt()
+        {
+            
+
+            return View();
+        }
+  [HttpPost]
+           public ActionResult TahminEt(int tahmin)
+           {
+           POST
+
+               int tutugumSayi = new Random().Next(10) + 1;
+
+               if (tahmin == tutugumSayi)
+               {
+
+                   return Content("Tebrikler,bildiniz");
+               }
+               else {
+
+                   return Content($"Malesef bilemediniz :(Tutugum Sayi{tutugumSayi} idi.");
+               }
+           }*/
     }
 }
